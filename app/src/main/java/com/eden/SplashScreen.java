dce73c4529826d99f4c71bf5ecf02ad05de62e66
Vadefolarin <Adedayovictor457@gmail.com>
2021-03-16 17:20:06 +0100

    last commit

--- app/src/main/java/com/eden/SplashScreen.java
+++ app/src/main/java/com/eden/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashScreen.this, HomePage.class);
+                Intent homeIntent = new Intent(SplashScreen.this, Home.class);
                 startActivity(homeIntent);
                 finish();
             }
